e1be3f557d1495a46971ed62c02a3d64c28fbf54
hajimestart46 <xinyuxia96@gmail.com>
2024-06-17 11:02:16 +0900

    first commit

---
+++ Script/PlayerScript.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [Header("Player Movement")]
+    public float playerSpeed = 1.9f;
+    public float playerSprint = 3f;
+
+    [Header("Player Script Cameras")]
+    public Transform playerCamera;
+
+    [Header("Player Animator and Gravity")]
+    public CharacterController cC;
+    public float gravity = -9.81f;
+
+    [Header("Player Jumping and velocity")]
+    public float turnCalmTime = 0.1f;
+    float turnCalmVelocity;
+    public float jumpRange = 1f;
+    Vector3 velocity;
+    public Transform surfaceCheck;
+    bool onSurface;
+    public float surfaceDistance = 0.4f;
+    public LayerMask surfaceMask;
+
+    private void Update()
+    {
+        onSurface = Physics.CheckSphere(surfaceCheck.position, surfaceDistance, surfaceMask);
+
+        if (onSurface && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        cC.Move(velocity * Time.deltaTime);
+
+        playerMove();
+        Jump();
+    }
+
+    void playerMove()
+    {
+        float horizontal_axis = Input.GetAxisRaw("Horizontal");
+        float vertical_axis = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal_axis, 0f, vertical_axis).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnCalmVelocity, turnCalmTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            // 调整速度以考虑是否按下冲刺键
+            // 速度を考慮し「shiftキー」に押す
+            float speed = Input.GetButton("Sprint") && onSurface ? playerSprint : playerSpeed;
+
+            cC.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+        else if (direction.magnitude < 0.1f)
+        {
+            cC.Move(Vector3.zero);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && onSurface)
+        {
+            velocity.y = Mathf.Sqrt(jumpRange * -2 * gravity); 
+        }
+    }
+}
